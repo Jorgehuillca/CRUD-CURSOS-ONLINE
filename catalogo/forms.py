15b503e3b54625fce3cf88dd6e6533869060be8b
Fernando Julca <fernandojulcap@gmail.com>
2025-08-05 18:42:46 -0500

    prueba

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -9,7 +9,7 @@ class CursoForm(forms.ModelForm):
     def clean_curso(self):
         curso = self.cleaned_data.get('curso', '').strip()
         self._validar_longitud(curso)
-        self._validar_reserbvado(curso)
+        self._validar_reservado(curso)
         return curso
     
     def _validar_longitud(self, curso):
@@ -18,11 +18,13 @@ class CursoForm(forms.ModelForm):
         if len(curso) < 3:
             raise forms.ValidationError('El nombre del curso debe tener al menos 3 caracteres.')
         
-    def _validar_reserbvado(self, curso):
+    def _validar_reservado(self, curso):
         
         # Validación de nombre reservado
         if curso.lower() == 'prueba':
             raise forms.ValidationError('El nombre "Prueba" está reservado para test.')
+        
+        
 
     def clean_duracion(self):
         duracion = self.cleaned_data.get('duracion')
